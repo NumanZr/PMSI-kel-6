ad364a137c49821c8bdfc3525c766d18ca2bc3e8
NumanZr <numan.zainulr18@mhs.uinjkt.ac.id>
2021-10-20 01:48:52 +0700

    haadeh

---
+++ views/sidebar_view.php
@@ -0,0 +1,87 @@
+<!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <br/>
+          <img src="../assets_style/image/user_1634052781.jpg" alt="" class="user-image" style="border:2px solid #fff;height:auto;width:100%;"/>
+        </div>
+
+        <div class="pull-left info" style="margin-top: 5px;">
+          <p>Nu'man Zr</p>
+          <p>Petugas</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+        
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+		
+        </div>
+        <ul class="sidebar-menu" data-widget="tree">
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <li class="header">MAIN NAVIGATION</li>
+            <li class="">
+                <a href="dashboard_view.php">
+                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="user/user_view.php" class="cursor">
+                    <i class="fa fa-user"></i> <span>Data Pengguna</span></a>
+			</li>
+			<li class="treeview menu-open">
+                <a href="#">
+                    <i class="fa fa-pencil-square"></i>
+                    <span>Data </span>
+                    <span class="pull-right-container">
+                    <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li class="">
+                        <a href="#" class="cursor">
+                            <span class="fa fa-book"></span> Data Buku
+                            
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="cursor">
+                            <span class="fa fa-tags"></span> Kategori
+                            
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="treeview menu-open">
+                <a href="#">
+                    <i class="fa fa-exchange"></i>
+                    <span>Transaksi</span>
+                    <span class="pull-right-container">
+                    <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li class="">
+                        <a href="#" class="cursor">
+                            <span class="fa fa-upload"></span> Peminjaman
+                            
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="#" class="cursor">
+                            <span class="fa fa-download"></span> Pengembalian
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="clearfix"></div>
+        <br/>
+        <br/>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
